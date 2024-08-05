b24bd8b9ba57612a22f2c6ef88d0bd684cb792ed
el0324 <126735482+el0324@users.noreply.github.com>
2024-08-04 23:33:09 -0700

    Add files via upload

---
+++ Basic.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal_I2C.h>
+/*
+* note:If lcd1602 uses PCF8574T, IIC's address is 0x27,
+* or lcd1602 uses PCF8574AT, IIC's address is 0x3F.
+*/
+LiquidCrystal_I2C lcd(0x27,16,2);
+void setup() {
+ if (!i2CAddrTest(0x27)) {
+ lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+ }
+ lcd.init(); // LCD driver initialization
+ lcd.backlight(); // Open the backlight
+ lcd.setCursor(0,0); // Move the cursor to row 0, column 0
+ lcd.print("hello world"); // The print content is displayed on the LCD
+}
+
+void loop() {}
+
+bool i2CAddrTest(uint8_t addr) {
+ Wire.begin();
+ Wire.beginTransmission(addr);
+ if (Wire.endTransmission() == 0) {
+ return true;
+ }
+ return false;
+}
---
+++ Scroll_Right.ino
@@ -0,0 +1,32 @@
+#include <LiquidCrystal_I2C.h>
+/*
+* note:If lcd1602 uses PCF8574T, IIC's address is 0x27,
+* or lcd1602 uses PCF8574AT, IIC's address is 0x3F.
+*/
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+char message[] = "hello world ";
+
+void setup() {
+ if (!i2CAddrTest(0x27)) {
+ lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+ }
+ lcd.init(); // LCD driver initialization
+ lcd.backlight(); // Open the backlight
+ lcd.setCursor(0,0); // Move the cursor to row 0, column 0
+ lcd.print(message); // The print content is displayed on the LCD
+}
+
+void loop() {
+  lcd.scrollDisplayRight();
+  delay(1000);
+}
+
+bool i2CAddrTest(uint8_t addr) {
+ Wire.begin();
+ Wire.beginTransmission(addr);
+ if (Wire.endTransmission() == 0) {
+ return true;
+ }
+ return false;
+}
